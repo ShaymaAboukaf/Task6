d42d3ad38d202056cf1a33a8c06b504bf7f74f13
ShaymaAboukaf <martincooper9988@gmail.com>
2024-09-12 21:34:03 +0300

    co

--- js/signup.js
+++ js/signup.js
@@ -126,3 +126,33 @@ function goToTab(tabNumber) {
   currentTab = tabNumber;
   showTab(currentTab);
 }
+
+// DROPDOWN MENU
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = document.querySelector(".select-btn"),
+  options = document.querySelectorAll(".option"),
+  sBtn_text = document.querySelector(".sBtn-text");
+
+const menus = document.querySelectorAll(".select-btn");
+
+menus.forEach((select) => {
+  select.addEventListener(
+    "click",
+    function (e) {
+      e.target.parentNode.classList.toggle("active-select");
+    },
+    true
+  );
+});
+
+options.forEach((option) => {
+  option.addEventListener("click", function (e) {
+    let selectedOption = e.target.innerText;
+    document.querySelector(".active-select .sBtn-text").innerText =
+      selectedOption;
+    document.querySelector(".active-select .sBtn-text").dataset.value =
+      selectedOption;
+
+    option.parentNode.parentNode.classList.remove("active-select");
+  });
+});
